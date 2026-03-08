948b7f2fb2cabae42720baf109af317e1f404724
tamim-devs <tamimh0727@gmail.com>
2026-03-08 19:34:30 +0600

    fix login btn

--- js/app.js
+++ js/app.js
@@ -13,6 +13,16 @@ tabs.forEach(tab => {
   });
 });
 
+function setActive(button){
+console.log(button);
+
+let buttons = document.querySelectorAll(".tab-btn")
+
+buttons.forEach(btn => btn.classList.remove("active"))
+
+button.classList.add("active")
+
+}
 
 async function fetchIssues(){
 container.innerHTML = "Loading..."
@@ -25,4 +35,52 @@ loadIssues(issues)
 
 }
 
+function filterIssues(type){
+
+let filtered = []
+
+if(type === "all"){
+filtered = issues
+}
+
+else if(type === "open"){
+filtered = issues.filter(issue => issue.status === "open")
+}
+
+else if(type === "closed"){
+filtered = issues.filter(issue => issue.status === "closed")
+}
+
+loadIssues(filtered)
+
+}function loadIssues(data){
+console.log(data);
+
+container.innerHTML = ""
+
+data.forEach(issue => {
+
+let borderColor = issue.status === "open" ? "green" : "purple"
+
+container.innerHTML += `
+
+<div class="card" onclick="showDetails(${issue.id})" style="border-top:4px solid ${borderColor}">
+
+<h3>${issue.title}</h3>
+
+<p>${issue.description}</p>
+
+<span class="tag">${issue.status}</span>
+
+<p>#${issue.id} by ${issue.author}</p>
+
+<p>${new Date(issue.createdAt).toLocaleDateString()}</p>
+
+</div>
+
+`
+
+})
+
+}
 fetchIssues()
